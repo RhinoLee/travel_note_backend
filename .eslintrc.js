cd1d0e76e4eabfa187b2d403d07d31b38394e4b4
RhinoLee <picklerhino@gmail.com>
2023-07-19 02:26:29 +0000

    setting eslint, prettier

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  plugins: ['prettier'],
+  env: {
+    node: true
+  },
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    'prettier/prettier': 'error'
+  }
+}
