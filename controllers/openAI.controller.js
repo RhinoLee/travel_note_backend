e270271a67df50ba0baf68d1fb0c31f6e31c29ce
RhinoLee <picklerhino@gmail.com>
2023-08-01 01:08:46 +0000

    openAI api error handler

--- controllers/openAI.controller.js
+++ controllers/openAI.controller.js
@@ -4,8 +4,8 @@ const orgBuf2Str = Buffer.from(OPENAI_ORG_KEY, 'utf8').toString()
 const apiBuf2Str = Buffer.from(OPENAI_API_KEY, 'utf8').toString()
 
 const configuration = new Configuration({
-  organization: orgBuf2Str,
-  apiKey: apiBuf2Str
+  organization: orgBuf2Str
+  // apiKey: apiBuf2Str
 })
 
 const openai = new OpenAIApi(configuration)
@@ -13,37 +13,51 @@ class openAIController {
   async completion(ctx) {
     const { query } = ctx.request.body
     console.log('query', query)
-    const completion = await openai.createChatCompletion({
-      model: 'gpt-3.5-turbo',
-      messages: [
-        { role: 'system', content: 'You are a helpful taiwan travel assistant.' },
-        {
-          role: 'user',
-          content: `${query}，請以 JSON 格式回答，如下，key-value 要隨著天數改變，地點數量不限：
-            {
-              "day1": [
-                "地點名稱 地址",
-                "地點名稱 地址",
-                "地點名稱 地址",
-                "地點名稱 地址"
-              ],
-              "day2": [
-                "地點名稱 地址",
-                "地點名稱 地址",
-                "地點名稱 地址"
-              ]
-            }`
-        }
-      ]
-    })
+    try {
+      const completion = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [
+          { role: 'system', content: 'You are a helpful taiwan travel assistant.' },
+          {
+            role: 'user',
+            content: `${query}，請以 JSON 格式回答，如下，key-value 要隨著天數改變，地點數量不限：
+              {
+                "day1": [
+                  "地點名稱 地址",
+                  "地點名稱 地址",
+                  "地點名稱 地址",
+                  "地點名稱 地址"
+                ],
+                "day2": [
+                  "地點名稱 地址",
+                  "地點名稱 地址",
+                  "地點名稱 地址"
+                ]
+              }`
+          }
+        ]
+      })
 
-    const result = JSON.parse(completion.data.choices[0].message.content)
+      const result = JSON.parse(completion.data.choices[0].message.content)
 
-    ctx.body = {
-      success: true,
-      data: result
+      ctx.body = {
+        success: true,
+        data: result
+      }
+    } catch (error) {
+      if (error.response) {
+        console.log(error.response.status)
+        console.log(error.response.data)
+      } else {
+        console.log(error.message)
+      }
+
+      ctx.status = 400
+      ctx.body = {
+        success: false,
+        data: error
+      }
     }
-    console.log('completion', completion)
   }
 }
 
