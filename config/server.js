611a680ad57ae2a7f52da2a5b3e3c6444dc40eab
RhinoLee <picklerhino@gmail.com>
2023-08-18 02:58:28 +0000

    export node env

--- config/server.js
+++ config/server.js
@@ -1,7 +1,6 @@
 const dotenv = require('dotenv')
 const envFile = process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development'
 dotenv.config({ path: envFile })
-dotenv.config()
 
 module.exports = { SERVER_PORT, MYSQL_DATABASE, MYSQL_USER, MYSQL_PASSWORD, MYSQL_HOST } =
   process.env
--- start.sh
+++ start.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+export NODE_ENV=production
+
 npm run migrate up
 if [ $? -ne 0 ]; then
   echo "Migration failed!"
   exit 1
 fi
+
 node ./main.js
