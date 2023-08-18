21889d15e58cdca2991dd8f2ede5464a29dd8c3c
RhinoLee <picklerhino@gmail.com>
2023-08-18 03:18:53 +0000

    setting proxy

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = require('./app')
 const { SERVER_PORT } = require('./config/server')
 
+app.proxy = true
 app.listen(SERVER_PORT, () => {
   console.log(`server is running at localhost port:${SERVER_PORT}`)
 })
